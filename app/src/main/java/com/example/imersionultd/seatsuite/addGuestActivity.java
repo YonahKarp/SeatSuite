8cf95473904bb4449a01488cc338800d5d5d24cc
Yonah Karp <YonahKarp@Yks-MacBook-Air.local>
2017-02-22 17:37:38 -0500

    added basic list functionality with delete (no add)

--- app/src/main/java/com/example/imersionultd/seatsuite/addGuestActivity.java
+++ app/src/main/java/com/example/imersionultd/seatsuite/addGuestActivity.java
@@ -1,5 +1,8 @@
 package com.example.imersionultd.seatsuite;
 
+import android.content.pm.ApplicationInfo;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +15,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class addGuestActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    static ArrayList<String> fruits = new ArrayList<>();
+    static ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +42,15 @@ public class addGuestActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +60,38 @@ public class addGuestActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        /**
+         * Set up listView w/ delete functionality
+         */
+        fruits.addAll(Arrays.asList("Apple", "Banana", "Cherry", "Kiwi", "Lemon", "Pear"));
+        listAdapter = new ArrayAdapter<String>(this, R.layout.list_item_guest,fruits);
+
+        //SwipeMenuList view from API
+        SwipeMenuListView listView = (SwipeMenuListView) findViewById(R.id.guestList);
+        listView.setMenuCreator(createSwipeMenu());
+        listView.setAdapter(listAdapter);
+
+
+        //on click of individual item
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(getApplicationContext(), fruits.get(position),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //for onClick of delete button
+        listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                fruits.remove(position);
+                listAdapter.notifyDataSetChanged(); //updates list
+
+                return true; //removes swipeView from screen
+            }
+        });
     }
 
     @Override
@@ -66,10 +118,10 @@ public class addGuestActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+        /*noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
@@ -97,5 +149,25 @@ public class addGuestActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void addGuest(MenuItem item){
+        Toast.makeText(this, "add Guest", Toast.LENGTH_SHORT).show();
+    }
+
+    //from API
+    public SwipeMenuCreator createSwipeMenu(){
+        return new SwipeMenuCreator() {
+            @Override
+            public void create(SwipeMenu menu) {
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(getApplicationContext());
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9, 0x3F, 0x25)));
+                deleteItem.setWidth(180);
+                deleteItem.setIcon(R.drawable.ic_delete);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
     }
 }
